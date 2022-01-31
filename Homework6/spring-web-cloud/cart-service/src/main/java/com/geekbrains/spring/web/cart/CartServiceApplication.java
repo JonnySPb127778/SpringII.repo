b60276f56dab8d78aaa2f1a3d022056fbdd13aaa
Engineer <aei.spb@gmail.com>
2022-01-31 17:25:54 +0300

    Spring II Yomework 6. Cart microService

---
+++ Homework6/spring-web-cloud/cart-service/src/main/java/com/geekbrains/spring/web/cart/CartServiceApplication.java
@@ -0,0 +1,11 @@
+package com.geekbrains.spring.web.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CartServiceApplication.class, args);
+	}
+}
